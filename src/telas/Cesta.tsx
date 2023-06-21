7dbae6fedc83109c6d44b19c0ae088f0f1d1c80f
Christian Pompeu <ochristianpompeu@gmail.com>
2023-06-20 23:33:41 -0300

    Fazendo os detalhes da cesta

--- src/telas/Cesta.tsx
+++ src/telas/Cesta.tsx
@@ -1,4 +1,4 @@
-import { Dimensions, Image, StyleSheet, Text } from "react-native";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 
 const screenWidth = Dimensions.get("screen").width;
 
@@ -7,6 +7,22 @@ function Cesta() {
     <>
       <Image source={require("../../assets/topo.png")} style={estilos.topo} />
       <Text style={estilos.titulo}>Detalhes da Cesta</Text>
+
+      <View style={estilos.cesta}>
+        <Text style={estilos.nomeCesta}>Nome da Cesta</Text>
+        <View style={estilos.fazenda}>
+          <Image
+            source={require("../../assets/logo.png")}
+            style={estilos.imagemFazenda}
+          />
+          <Text style={estilos.nomeFazenda}>Nome da Fazenda</Text>
+        </View>
+        <Text style={estilos.descricaoCesta}>
+          Uma descrição razoavel da fazenda par ser exibida para os usuários
+          finais
+        </Text>
+        <Text style={estilos.precoCesta}>R$ 40,00</Text>
+      </View>
     </>
   );
 }
@@ -27,5 +43,40 @@ const estilos = StyleSheet.create({
     fontWeight: "bold",
     padding: 16,
     color: "#FFFFFF",
+  },
+  cesta: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  nomeCesta: {
+    lineHeight: 46,
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#464646",
+  },
+  fazenda: {
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
+  imagemFazenda: {
+    width: 32,
+    height: 32,
+  },
+  nomeFazenda: {
+    lineHeight: 26,
+    fontSize: 16,
+    marginLeft: 12,
+  },
+  descricaoCesta: {
+    lineHeight: 26,
+    fontSize: 16,
+    color: "#A3A3A3",
+  },
+  precoCesta: {
+    color: "#2A9F85",
+    lineHeight: 42,
+    fontWeight: "bold",
+    fontSize: 26,
+    marginTop: 8,
   },
 });
