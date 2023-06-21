9fc7c8d6ca034b5fde0322215242765d9de9d838
Christian Pompeu <ochristianpompeu@gmail.com>
2023-06-20 22:54:58 -0300

    Fazendo o topo da página

--- src/telas/Cesta.tsx
+++ src/telas/Cesta.tsx
@@ -1,7 +1,31 @@
-import { Image } from "react-native";
+import { Dimensions, Image, StyleSheet, Text } from "react-native";
+
+const screenWidth = Dimensions.get("screen").width;
 
 function Cesta() {
-  return <Image source={require("../../assets/topo.png")} />;
+  return (
+    <>
+      <Image source={require("../../assets/topo.png")} style={estilos.topo} />
+      <Text style={estilos.titulo}>Detalhes da Cesta</Text>
+    </>
+  );
 }
 
 export { Cesta };
+
+const estilos = StyleSheet.create({
+  topo: {
+    width: "100%",
+    height: (578 / 768) * screenWidth,
+  },
+  titulo: {
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    lineHeight: 26,
+    fontSize: 16,
+    fontWeight: "bold",
+    padding: 16,
+    color: "#FFFFFF",
+  },
+});
